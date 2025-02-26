f9c806e8dff76e570258aaedc73f586ba7c29a93
GIRMA <girma@girma.localdomain>
2025-02-25 19:02:58 -0500

    full crud api design and manage

---
+++ Back-end/model/recipe.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const RecipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true, 
+  },
+  category:{
+    type:String,
+    required : true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true, 
+  },
+  instructions: {
+    type: [String], 
+    required: true, 
+  },
+  imageUrl: {
+    type: String, 
+  },
+  authorId: {
+    type: String, 
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now, 
+  },
+  dateModified: {
+    type: Date,
+    default: Date.now,
+  },
+  tags: {
+    type: [String], 
+  },
+});
+
+module.exports = mongoose.model('Recipe', RecipeSchema);
